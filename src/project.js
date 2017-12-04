b01b968540f178638fc8e139f3c16cdb453af668
Yalin(Lynne)NEU <lynne@AppledeMacBook-Pro.local>
2017-12-03 23:18:48 -0800

    1st commit

---
+++ src/project.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+	$("#panjiva-img").mouseover(function(){
+		$("#panjiva-img").css("opacity","1");
+	});
+});
+
+$(document).ready(function(){
+    $("#kk-img").mouseover(function(){
+        $("#kk-img").css("opacity","1");
+    });
+});
+
+$(document).ready(function(){
+    $("#neu-img").mouseover(function(){
+        $("#neu-img").css("opacity","1");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#panjiva-img").mouseout(function(){
+        $("#panjiva-img").css("opacity","0.5");
+    });
+});
+
+$(document).ready(function(){
+    $("#kk-img").mouseout(function(){
+        $("#kk-img").css("opacity","0.5");
+    });
+});
+
+$(document).ready(function(){
+    $("#neu-img").mouseout(function(){
+        $("#neu-img").css("opacity","0.5");
+    });
+});
